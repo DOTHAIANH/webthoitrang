cc88e142b0eecc03458cbe9df0031a6212f05bf1
DOTHAIANH <97963238+DOTHAIANH@users.noreply.github.com>
2023-03-16 01:56:01 +0700

    aaaaa

--- Web thời trang/js/slick.js
+++ Web thời trang/js/slick.js
@@ -61,8 +61,26 @@ $(document).ready(function() {
     dots: false,
     arrows: true,
     prevArrow:"<button type='button' class='slick-prev pull-left slick-arrow'><i class='fa-solid fa-angle-left'></i></button>",
-    nextArrow:"<button type='button' class='slick-next pull-right slick-arrow'><i class='fa-solid fa-angle-right'></i></button>"
+    nextArrow:"<button type='button' class='slick-next pull-right slick-arrow'><i class='fa-solid fa-angle-right'></i></button>",
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
+
+
   // chi tiet san pham
   $('.detail-product-slide-left').slick({
     slidesToShow: 1,
